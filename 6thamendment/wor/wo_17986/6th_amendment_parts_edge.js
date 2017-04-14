36dd7af1930505e181157b46fbf83c5517c1224e
Mark A. Trickett <mtrickett@montpelier.org>
2017-04-14 17:11:10 -0400

    1st commit
    1st commit

---
+++ 6thamendment/wor/wo_17986/6th_amendment_parts_edge.js
@@ -0,0 +1,57 @@
+
+(function(compId){var _=null,y=true,n=false,x1='6.0.0',x6='43',x15='rgba(255,255,255,0.00)',x2='5.0.0',x22='ellipse',x13='rgba(0,0,0,0)',g='image',e21='${rct_ul_two}',b='block',x39='rgba(0,0,0,0.65098)',x30='true',e20='${two}',x27='EllipseCopy',x33='rgba(121,18,25,1.00)',x12='19',x='text',x7='marvel, sans-serif',x42='Ellipse',m='rect',x10='rgba(255,255,255,1.00)',e41='${EllipseCopy}',x40='TextCopy2',e46='${Ellipse}',i='none',x29='rgba(192,192,192,1)',x38='nowrap',e19='${rct_ul_one}',w='width',p='px',e18='${one}',lf='left',x4='rgba(0,0,0,1.00)',x36='17px',x32='rgba(154,23,31,1.00)',x28='pointer',po='center',x44='58px',x23='50%',x35='50px',x31='farthest-corner',l='normal',x26='auto',x25='133px',x45='Text',x24='0px',x9='break-word',x3='6.0.0.400',x8='400',xc='rgba(0,0,0,1)',x37='rgba(235,153,57,1.00)',d='display';var g14='close_btn_image.png';var s11="<p style=\"margin: 0px;\">The first part of the 6th Amendment guarantees a speedy and public trial, helping to ensure that those accused of crimes will not be imprisoned indefinitely while waiting for their day in court, and that no secret proceedings will be held against them. The 6th Amendment then guarantees that criminal trials will be heard by an impartial jury selected from the district where the crime was committed. Trial by jury had long been a recognized right under Common Law, but the 6th Amendment provided for further stipulations that help ensure the farness of such trial arrangements.</p>",s34="<p style=\"margin: 0px;\">2</p>",s17="<p style=\"margin: 0px;\">Next, the 6th Amendment guarantees that those accused of crimes shall be informed of who is accusing them, and why. This right is important because it allows a criminal defendant to respond appropriately to the charges laid against them, and to prepare a case to argue for their own innocence. We will go more into that in this chapter. While making that case, the 6th Amendment also ensures that defendants are able to confront witnesses against them and compel witnesses to appear on their behalf. Finally, as we will discuss in detail, the 6th Amendment guarantees the all-important right to counsel in criminal proceedings, meaning that defendants may have a lawyer to represent them in court, unless they decline to have representation.</p>",s16="<p style=\"margin: 0px;\">Part II</p>",s5="<p style=\"margin: 0px;\">Part 1</p>",s43="<p style=\"margin: 0px;\">1</p>";var im='images/',aud='media/',vid='media/',js='js/',fonts={'tangerine, sans-serif':'<script src=\"http://use.edgefonts.net/tangerine:n4,n7:all.js\"></script>','marvel, sans-serif':'<script src=\"http://use.edgefonts.net/marvel:i4,n7,n4,i7:all.js\"></script>'},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'sym_btn1',symbolName:'sym_btn3',t:m,r:['839','33','133','133','auto','auto']},{id:'sym_btn2',symbolName:'sym_btn2',t:m,r:['839','200px','133','133','auto','auto']},{id:'one',v:b,t:'group',r:['-791px','290','791','275','auto','auto'],c:[{id:'bg_part_one',t:m,r:['0px','0px','791px','275px','auto','auto'],f:[x4],s:[0,"rgb(0, 0, 0)",i]},{id:'ttl_part_one',t:x,r:['17px','9px','102px','55px','auto','auto'],text:s5,align:"left",n:[x7,[x6,p],"rgba(154,23,31,1.00)",x8,i,l,x9,l],ts:["","","","",i]},{id:'rct_ul_one',v:b,t:m,r:['18px','61px','0px','2px','auto','auto'],f:[x10],s:[0,"rgb(0, 0, 0)",i]},{id:'txt_part_one',t:x,r:['17px','75px','720px','171px','auto','auto'],text:s11,align:"left",n:[x7,[x12,p],"rgba(235,153,57,1)",x8,i,l,x9,l],ts:["","","","",i]},{id:'close_btn_image',t:g,r:['737px','0px','54px','54px','auto','auto'],f:[x13,im+g14,'0px','0px']},{id:'btn_one_reverse',t:m,r:['0px','0px','791px','275px','auto','auto'],cu:'pointer',f:[x15],s:[0,"rgb(0, 0, 0)",i]}]},{id:'two',v:b,t:'group',r:['-791px','290','791','275','auto','auto'],c:[{id:'bg_part_two',t:m,r:['0px','0px','791px','275px','auto','auto'],f:[x4],s:[0,"rgb(0, 0, 0)",i]},{id:'ttl_part_two',t:x,r:['17px','9px','102px','55px','auto','auto'],text:s16,align:"left",n:[x7,[x6,p],"rgba(154,23,31,1.00)",x8,i,l,x9,l],ts:["","","","",i]},{id:'rct_ul_two',v:b,t:m,r:['18px','61px','0px','2px','auto','auto'],f:[x10],s:[0,"rgb(0, 0, 0)",i]},{id:'txt_part_two',t:x,r:['17px','75px','720px','171px','auto','auto'],text:s17,align:"left",n:[x7,[x12,p],"rgba(235,153,57,1)",x8,i,l,x9,l],ts:["","","","",i]},{id:'close_btn_imageCopy',t:g,r:['737px','0px','54px','54px','auto','auto'],f:[x13,im+g14,'0px','0px']},{id:'btn_two_reverse',t:m,r:['0px','0px','791px','275px','auto','auto'],cu:'pointer',f:[x15],s:[0,"rgb(0, 0, 0)",i]}]}],style:{'${Stage}':{isStage:true,r:['null','null','1024px','565px','auto','auto'],overflow:'hidden',f:[x15]}}},tt:{d:6792,a:y,l:{"one":1000,"one_reverse":2500,"two":4000,"two_reverse":5500},data:[["eid14",d,3801,0,"easeInOutQuart",e18,b,i],["eid6",lf,1000,750,"easeInOutQuart",e18,'-791px','0px'],["eid11",lf,3000,750,"easeInOutQuart",e18,'0px','-791px'],["eid8",w,1750,500,"easeInOutQuart",e19,'0px','720px'],["eid12",w,2500,500,"easeInOutQuart",e19,'720px','0px'],["eid21",d,6792,0,"easeInOutQuart",e20,b,i],["eid16",lf,4000,750,"easeInOutQuart",e20,'-791px','0px'],["eid19",lf,6000,750,"easeInOutQuart",e20,'0px','-791px'],["eid22",d,6792,0,"easeInOutQuart",e21,b,i],["eid18",w,4750,500,"easeInOutQuart",e21,'0px','720px'],["eid20",w,5500,500,"easeInOutQuart",e21,'720px','0px'],["eid13",d,3801,0,"easeInOutQuart",e19,b,i]]}},"sym_btn2":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:x22,br:[x23,x23,x23,x23],r:[x24,x24,x25,x25,x26,x26],id:x27,s:[0,xc,i],cu:x28,f:[x29,[50,50,x30,x31,[[x32,0],[x33,100]]]]},{t:x,align:po,text:s34,cu:x28,r:[x35,x36,x26,x26,x26,x26],n:[x7,[82,p],x37,l,i,'',x9,x38],textShadow:[x39,3,3,5],id:x40}],style:{'${symbolSelector}':{r:[_,_,x25,x25]}}},tt:{d:625,a:y,l:{"btn2_rollover":250,"btn2_rollout":500},data:[["eid3","background-image",250,126,"linear",e41,[50,50,true,'farthest-corner',[['rgba(154,23,31,1.00)',0],['rgba(121,18,25,1.00)',100]]],[50,50,true,'farthest-corner',[['rgba(215,30,41,1.00)',0],['rgba(121,18,25,1.00)',100]]]],["eid4","background-image",500,125,"linear",e41,[50,50,true,'farthest-corner',[['rgba(215,30,41,1.00)',0],['rgba(121,17,24,1.00)',100]]],[50,50,true,'farthest-corner',[['rgba(154,23,31,1.00)',0],['rgba(121,17,24,1.00)',100]]]]]}},"sym_btn3":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:x22,br:[x23,x23,x23,x23],r:[x24,x24,x25,x25,x26,x26],id:x42,s:[0,xc,i],cu:x28,f:[x29,[50,50,x30,x31,[[x32,0],[x33,100]]]]},{t:x,align:po,text:s43,cu:x28,r:[x44,x36,x26,x26,x26,x26],n:[x7,[82,p],x37,l,i,'',x9,x38],textShadow:[x39,3,3,5],id:x45}],style:{'${symbolSelector}':{r:[_,_,x25,x25]}}},tt:{d:624,a:y,l:{"btn1_rollover":250,"btn1_rollout":500},data:[["eid1","background-image",250,123,"linear",e46,[50,50,true,'farthest-corner',[['rgba(154,23,31,1.00)',0],['rgba(121,18,25,1.00)',100]]],[50,50,true,'farthest-corner',[['rgba(215,30,41,1.00)',0],['rgba(121,18,25,1.00)',100]]]],["eid2","background-image",500,124,"linear",e46,[50,50,true,'farthest-corner',[['rgba(215,30,41,1.00)',0],['rgba(121,17,24,1.00)',100]]],[50,50,true,'farthest-corner',[['rgba(154,23,31,1.00)',0],['rgba(121,17,24,1.00)',100]]]]]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-17859899");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${sym_btn1}","mouseover",function(sym,e){sym.getSymbol("sym_btn1").play("btn1_rollover");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${sym_btn1}","mouseout",function(sym,e){sym.getSymbol("sym_btn1").play("btn1_rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${sym_btn1}","click",function(sym,e){sym.play("one");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${sym_btn2}","mouseover",function(sym,e){sym.getSymbol("sym_btn2").play("btn2_rollover");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${sym_btn2}","mouseout",function(sym,e){sym.getSymbol("sym_btn2").play("btn2_rollout");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${sym_btn2}","click",function(sym,e){sym.play("two");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${btn_two_reverse}","click",function(sym,e){sym.play("two_reverse");});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${btn_one_reverse}","click",function(sym,e){sym.play("one_reverse");});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",2250,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",3750,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",5250,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",6750,function(sym,e){sym.stop();});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'sym_btn2'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",376,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",625,function(sym,e){sym.stop();});
+//Edge binding end
+})("sym_btn2");
+//Edge symbol end:'sym_btn2'
+
+//=========================================================
+
+//Edge symbol: 'sym_btn3'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",0,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",373,function(sym,e){sym.stop();});
+//Edge binding end
+Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",624,function(sym,e){sym.stop();});
+//Edge binding end
+})("sym_btn3");
+//Edge symbol end:'sym_btn3'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-17859899");
